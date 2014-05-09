f770601549a47a929d17e3bfb0535dd9f9eca3ff
offff <isvind23@gmail.com>
2014-05-10 01:34:13 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,7 +4,5 @@ mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
 myData <- read.csv.sql('household_power_consumption.txt',mySql, sep=";")
 
 Data <-myData
-Data$Date <-as.Date(Data$Date, format="%d/%m/%Y")
-Data$Time <-strptime(Data$Time, format="%H:%M:%S")
 
 hist(Data$Global_active_power, col='red', main='Global Active Power', xlab='Global Active Power (kilowatts)', ylab='Frequency')
